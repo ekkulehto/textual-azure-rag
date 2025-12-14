7aa382b4d71f75ed77d94989622c68dbde9f478b
Ekku Lehto <ekkulehto@proton.me>
2025-12-14 15:00:20 +0200

    feat: Build basic app layout

--- app.py
+++ app.py
@@ -1,43 +1,79 @@
 import os
 from dotenv import load_dotenv
-from pyfiglet import figlet_format
+from textual_pyfiglet import FigletWidget 
+from openai import AzureOpenAI
+
 from textual.app import App, ComposeResult
-from textual.widgets import TextArea, Footer, Header, Static, Button
-from textual.containers import HorizontalGroup, VerticalGroup
+from textual.containers import Container, VerticalScroll
+from textual.widgets import Input, Markdown, Header
+
+
+from rag_system_prompt import RAG_SYSTEM_PROMPT
 
 load_dotenv()
 
-AZURE_ENDPOINT = os.getenv('AZURE_ENDPOINT')
-API_KEY = os.getenv('API_KEY ')
-DEPLOYMENT_NAME = os.getenv('DEPLOYMENT_NAME')
-API_VERSION = os.getenv('API_VERSION')
+AZURE_ENDPOINT = os.getenv("AZURE_ENDPOINT")
+AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
+DEPLOYMENT_NAME = os.getenv("DEPLOYMENT_NAME")
+API_VERSION = os.getenv("API_VERSION")
+
+SEARCH_ENDPOINT = os.getenv("SEARCH_ENDPOINT")
+SEARCH_KEY = os.getenv("SEARCH_KEY")
+SEARCH_INDEX_NAME = os.getenv("SEARCH_INDEX_NAME")
+SEMANTIC_CONFIGURATION_NAME = os.getenv("SEMANTIC_CONFIGURATION_NAME")
 
 
-class ChatContainer(VerticalGroup):
+class ChatMessage(Markdown):
+    """One chat message"""
+
+    def update_text(self, new_text: str) -> None:
+        self.update(new_text)
+
+
+class ChatContainer(VerticalScroll):
+    """Scrollable chat container."""
     pass
 
-class Logo(Static):
-    def __init__(self) -> None:
-        text = figlet_format("GymRAG", font="larry3d")
-        super().__init__(text, id="logo")
 
+class InputArea(Container):
+    """User Input"""
 
-class UserTextArea(HorizontalGroup):
     def compose(self) -> ComposeResult:
-        yield TextArea(placeholder="What do you want to know?", id="input")
-        # yield Button("Send", id="send", variant="success")
+        yield Input(
+            placeholder="Type your message and press enter...",
+            id="prompt",
+        )
 
 
-class Chatbot(App):
+class GymRAG(App):
     CSS_PATH = "style.tcss"
 
+    def on_mount(self) -> None:
+        """Run when the app starts."""
+        self.client = AzureOpenAI(
+            azure_endpoint=AZURE_ENDPOINT,
+            api_key=AZURE_OPENAI_API_KEY,
+            api_version=API_VERSION,
+        )
+
+        self.conversation_history = [
+            {
+                "role": "system",
+                "content": RAG_SYSTEM_PROMPT
+            }
+        ]
+
+        self.first_message_sent = False
+
     def compose(self) -> ComposeResult:
-        yield Footer(show_command_palette=False)
+        """Build the UI."""
         yield Header()
-        yield Logo()
-        yield UserTextArea()
+        yield ChatContainer(id="chat-container")
+        yield FigletWidget("GymRAG", font="ansi_regular", id="logo")
+        yield InputArea(id="input-area")
+
 
 
 if __name__ == "__main__":
-    app = Chatbot()
+    app = GymRAG()
     app.run()
